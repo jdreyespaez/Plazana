290c88e083966e3a8d8de7404215b6cd4b7c1962
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-03-06 12:03:12 -0500

    S5: Pantalla de Detalle para mostrar desde cualquier botón Abrir Detalle

---
+++ pages/Detalles.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+
+export default class DetailsScreen extends React.Component {
+  //S5: Pantalla de Detalle para mostrar desde cualquier botón Abri Detalle
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Detalles!</Text>
+      </View>
+    );
+  }
+}
