d777eaf8eb90d21524495846f6a9ea0c51cd4eb0
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-03-06 13:54:37 -0500

    S7: Pantalla de Perfil para mostrar al abrir el botón Abrir Perfil

---
+++ pages/Perfil.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+
+export default class ProfileScreen extends React.Component {
+  //S7: Pantalla de Perfil para mostrar al abrir el botón Abrir Perfil
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Profile!</Text>
+      </View>
+    );
+  }
+}
